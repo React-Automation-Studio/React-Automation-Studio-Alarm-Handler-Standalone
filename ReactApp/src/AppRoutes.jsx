847280956fa84c746f5905a6cd70cb377d6887b3
William Duckitt <wdduckitt@sun.ac.za>
2024-02-06 10:55:18 +0200

    Refactor routes in AppRoutes.jsx

--- ReactApp/src/AppRoutes.jsx
+++ ReactApp/src/AppRoutes.jsx
@@ -2,38 +2,19 @@ import React, { useContext } from "react";
 
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 
-// demos
-import MainDashboard from "React-Automation-Studio/components/UI/MainDashboard";
-// import Demos from "./components/demos/Demos";
-import EpicsDemos from "React-Automation-Studio/components/Examples/EpicsDemos";
-import MobileDemo1 from "React-Automation-Studio/components/Examples/Mobile/MobileDemo1";
-import MobileDemo2 from "React-Automation-Studio/components/Examples/Mobile/MobileDemo2";
-import BeamlineControlSystem from "React-Automation-Studio/components/ControlScreens/BeamlineControlSystem";
-import TableControlSystem from "React-Automation-Studio/components/ControlScreens/TableControlSystem";
-import Test3D from "React-Automation-Studio/components/Experimental/Test3D";
-import AlarmHandlerDemo from "React-Automation-Studio/components/Examples/AlarmHandlerDemo";
-import LoadSaveExample from "React-Automation-Studio/components/ExperimentalExamples/LoadSaveExample";
-import ArchiverDataViewerDemo from "React-Automation-Studio/components/ArchiverDataViewer/ArchiverDataViewerDemo";
-import SettingsSteererXY from "React-Automation-Studio/components/SettingsPages/SettingsSteererXY";
-import SettingsSinglePS from "React-Automation-Studio/components/SettingsPages/SettingsSinglePS";
-import AdvancedSettingsSinglePS from "React-Automation-Studio/components/ExperimentalControlScreens/SettingsPages/AdvancedSettingsSinglePS";
-import Vault from "React-Automation-Studio/components/AlarmHandler/Vault";
-import AreaDetectorSimExample from "React-Automation-Studio/components/Examples/AreaDetectorSimExample";
-//staging
-import Staging from "./components/staging/Staging";
-import Example from "./components/staging/Example/Example";
-import Example1 from "./components/staging/Example/Example1";
-import Example2 from "./components/staging/Example/Example2";
-import Example3 from "./components/staging/Example/Example3";
+/// demos
+import MainDashboard from './React-Automation-Studio/components/UI/MainDashboard';
+import Vault from './React-Automation-Studio/components/AlarmHandler/Vault';
 //system
-import Login from "React-Automation-Studio/components/SystemComponents/Login";
-import Probe from "React-Automation-Studio/components/SettingsPages/Probe";
-import Help from "React-Automation-Studio/components/docs/Help";
+import Login from "./React-Automation-Studio/components/SystemComponents/Login";
+import Probe from "./React-Automation-Studio/components/SettingsPages/Probe";
+import Help from "./React-Automation-Studio/components/docs/Help";
 import AppMain from "./AppMain";
 import AutomationStudioContext from "React-Automation-Studio/components/SystemComponents/AutomationStudioContext";
-import Administrator from "React-Automation-Studio/components/Administrator/Administrator";
+import Administrator from "React-Automation-Studio/components/Administrator/Administrator.js";
 import UserProfile from "React-Automation-Studio/components/SystemComponents/userProfiles/UserProfile";
 import ProtectedRoute from "React-Automation-Studio/components/SystemComponents/ProtectedRoute";
+
 
 const AppRoutes = (props) => {
   const context = useContext(AutomationStudioContext);
@@ -85,7 +66,7 @@ const AppRoutes = (props) => {
             path="/Login"
             component={() => (
               <Login
-                version="V4.0.3"
+                version="V5.0.0"
                 timeout={5000}
               />
             )}
@@ -125,67 +106,7 @@ const AppRoutes = (props) => {
         />
         {/*system end*/}
 
-        {/*demos start*/}
-        {enableDemos && (
-          <Route
-          exact
-          path="/DemosDashboard"
-          element={
-            <ProtectedRoute>
-              <MainDashboard />
-            </ProtectedRoute>
-          }
-        />
-        )}
-        {/* {enableDemos && <ProtectedRoute path="/Demos" component={Demos} />} */}
-        {enableDemos && (
-           <Route
-           path="/MobileDemo1"
-           element={
-             <ProtectedRoute>
-               <MobileDemo1 />
-             </ProtectedRoute>
-           }
-         />)}
-        {enableDemos && (
-          <Route
-          path="/MobileDemo2"
-          element={
-            <ProtectedRoute>
-              <MobileDemo2 />
-            </ProtectedRoute>
-          }
-        />
-        )}
-        {enableDemos && (
-           <Route
-           path="/EpicsDemos"
-           element={
-             <ProtectedRoute>
-               <EpicsDemos />
-             </ProtectedRoute>
-           }
-         />
-        )}
-        {enableDemos && <Route
-          path="/Test3D"
-          element={
-            <ProtectedRoute>
-              <Test3D />
-            </ProtectedRoute>
-          }
-        />}
-
-        {enableDemos && (
-           <Route
-          path="/AlarmHandlerDemo"
-          element={
-            <ProtectedRoute>
-              <AlarmHandlerDemo />
-            </ProtectedRoute>
-          }
-        />
-        )}
+        
         {enableDemos &&  <Route
           path="/VaultDemo"
           element={
@@ -194,132 +115,7 @@ const AppRoutes = (props) => {
             </ProtectedRoute>
           }
         />}
-        {enableDemos && (
-           <Route
-           path="/LoadSaveExample"
-           element={
-             <ProtectedRoute>
-               <LoadSaveExample />
-             </ProtectedRoute>
-           }
-         />
-        )}
-        {enableDemos && (
-           <Route
-           path="/ArchiverDataViewerDemo"
-           element={
-             <ProtectedRoute>
-               <ArchiverDataViewerDemo />
-             </ProtectedRoute>
-           }
-         />
-        )}
-        {/* new Beamline and table control System routes start*/}
-        {enableDemos && (
-         <Route
-         path="/BeamlineControlSystem"
-         element={
-           <ProtectedRoute>
-             <BeamlineControlSystem />
-           </ProtectedRoute>
-         }
-       />
-        )}
-        {enableDemos && (
-          <Route
-          path="/AdvancedSettingsSinglePS"
-          element={
-            <ProtectedRoute>
-              <AdvancedSettingsSinglePS />
-            </ProtectedRoute>
-          }
-        />
-        )}
-        {enableDemos && (
-           <Route
-           path="/TableControlSystem"
-           element={
-             <ProtectedRoute>
-               <TableControlSystem />
-             </ProtectedRoute>
-           }
-         />
-        )}
-        {enableDemos && (
-          <Route
-          path="/SettingsSinglePS"
-          element={
-            <ProtectedRoute>
-              <SettingsSinglePS />
-            </ProtectedRoute>
-          }
-        />
-        )}
-        {enableDemos && (
-        <Route
-        path="/SettingsSteererXY"
-        element={
-          <ProtectedRoute>
-            <SettingsSteererXY />
-          </ProtectedRoute>
-        }
-      />
-        )}
-
-        {/* new Beamline and table control System routes end*/}
-
-        {/*demos end*/}
-
-        {/*staging start*/}
-        <Route
-          path="/Staging"
-          element={
-            <ProtectedRoute>
-              <Staging />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/Example"
-          element={
-            <ProtectedRoute>
-              <Example />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/Example1"
-          element={
-            <ProtectedRoute>
-              <Example1 />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/Example2"
-          element={
-            <ProtectedRoute>
-              <Example2 />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/Example3"
-          element={
-            <ProtectedRoute>
-              <Example3 />
-            </ProtectedRoute>
-          }
-        />
-        {/*staging end*/}
-        <Route
-          path="/AreaDetectorSimExample"
-          element={
-            <ProtectedRoute>
-              <AreaDetectorSimExample />
-            </ProtectedRoute>
-          }
-        />
+       
       </Routes>
     </BrowserRouter>
   );
